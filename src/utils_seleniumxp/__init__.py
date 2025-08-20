2c9ae52d329bfab98fe4cbbc3b254b0acdbca580
dornech <dornech@users.noreply.github.com>
2025-08-20 02:13:48 +0200

    feat: include new version of hatch-vcs-footgun
    adjust __init__.py, and pyproject.toml, include version.py, details see https://github.com/maresb/hatch-vcs-footgun-example

--- src/utils_seleniumxp/__init__.py
+++ src/utils_seleniumxp/__init__.py
@@ -58,38 +58,19 @@ Beside the factory function to create the webdriver there is no change in the AP
 
 
 
-from importlib.metadata import PackageNotFoundError, version
+# version determination
 
+# original Hatchlor version
+# from importlib.metadata import PackageNotFoundError, version
 # try:
-#     __version__ = version('utils-seleniumxp')
+#     __version__ = version('{{ cookiecutter.project_slug }}')
 # except PackageNotFoundError:  # pragma: no cover
 #     __version__ = 'unknown'
 # finally:
 #     del version, PackageNotFoundError
 
-# up-to-date version tag for modules installed in editable mode inspired by
-# https://github.com/maresb/hatch-vcs-footgun-example/blob/main/hatch_vcs_footgun_example/__init__.py
-# Define the variable '__version__':
-try:
-
-    # own developed alternative variant to hatch-vcs-footgun overcoming problem of ignored setuptools_scm settings
-    # from hatch-based pyproject.toml libraries
-    from hatch.cli import hatch
-    from click.testing import CliRunner
-    # determine version via hatch
-    __version__ = CliRunner().invoke(hatch, ["version"]).output.strip()
-
-except (ImportError, LookupError):
-    # As a fallback, use the version that is hard-coded in the file.
-    try:
-        from ._version import __version__  # noqa: F401
-    except ModuleNotFoundError:
-        # The user is probably trying to run this without having installed the
-        # package, so complain.
-        raise RuntimeError(
-            f"Package {__package__} is not correctly installed. Please install it with pip."
-        )
-
+# latest import requirement for hatch-vcs-footgun-example
+from utils_seleniumxp.version import __version__
 
 
 # import Python bindings for Selenium webdriver
